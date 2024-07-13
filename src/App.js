20b8380545cdbf3deb77d0e5176344e1e3cbb1b3
vidur2 <vmod2005@gmail.com>
2024-07-13 18:05:58 +0200

    tmp

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
     <div>
     <div className="App" style={{ width: "96%", borderRadius: "10px", marginLeft: "auto", marginRight: "auto",  fontFamily: "SpaceMono-Regular"}}>
       <img alt="logo here" style={{float: "left", marginTop: "3%", marginLeft: "4%"}} src={logoPlaceholder} height={75}></img>
-      <a  href="/sign_up" style={{paddingTop: "0.5%", paddingBottom: "0.5%", paddingLeft: "1%", paddingRight: "1%", fontWeight: "normal", fontSize: "100%", marginTop: "3%", float: "right", marginRight: "4%", borderRadius: "5px", borderStyle: "solid", borderColor: "transparent", backgroundColor: "#09E482", color: "black", textDecoration: "none"}} onClick={handlePageChange}>Sign Up</a>
-      <h1 style={{marginTop: "15%", fontSize: "500%", float: "left", width: "50%", textAlign: "left", fontWeight: "normal", fontWeight: "600"}}>A social gym app that meets your logging needs</h1>
-      <div style={{ float: "left", clear: "left", backgroundColor: "#121619", borderRadius: "10px", padding: "1% 2%", marginLeft: "11%", marginTop: "2%", marginBottom: "3%", display: "inline-block", whitespace: "nowrap", width: '30%'}}>
+      {/* <a  href="/sign_up" style={{paddingTop: "0.5%", paddingBottom: "0.5%", paddingLeft: "1%", paddingRight: "1%", fontWeight: "normal", fontSize: "100%", marginTop: "3%", float: "right", marginRight: "50%", borderRadius: "5px", borderStyle: "solid", borderColor: "transparent", backgroundColor: "#09E482", color: "black", textDecoration: "none"}} onClick={handlePageChange}>Sign Up</a> */}
+      <h1 style={{marginTop: "15%", fontSize: "500%", float: "left", width: "50%", textAlign: "left", fontWeight: "normal", marginLeft: "-10%"}}>A social gym app that meets your logging needs</h1>
+      <div style={{ float: "left", clear: "left", backgroundColor: "#121619", borderRadius: "10px", padding: "1% 2%", marginLeft: "6%", marginTop: "2%", marginBottom: "3%", display: "inline-block", whitespace: "nowrap", width: '30%'}}>
         <form style={{display: "inline-block"}}>
         <Stack direction="row" spacing={2}>
         <input type="text" id="workEmail" className='float-child' style={{ backgroundColor: "transparent", width: 'fit-content', borderStyle: "none", color: "white", display: "inline-block", fontSize: "15px", float: "left", marginLeft: "-40%"}} placeholder='Email Address here'></input>
@@ -26,7 +26,7 @@ function App() {
         </form>
       </div>
     </div>
-    <img src={appPage} width="20%" style={{float: "right", marginRight: "15%", marginTop: "-30%"}}></img>
+    <img src={appPage} width="20%" style={{float: "right", marginRight: "7%", marginTop: "-30%"}}></img>
 
   </div>
   );
